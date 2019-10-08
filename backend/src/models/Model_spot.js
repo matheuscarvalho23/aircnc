0cfd6e13c2f395383c511fb97322609924508d9d
conesnoopy23 <matheus.santos.hcs@gmail.com>
2019-10-07 22:49:22 -0300

    Finish web interface

--- backend/src/models/Model_spot.js
+++ backend/src/models/Model_spot.js
@@ -9,6 +9,14 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Model_user'
     }
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+   return `http://localhost:3333/files/${this.thumbnail}`; 
 });
 
 module.exports = mongoose.model('Model_spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express  = require('express');
 const mongoose = require('mongoose');
+const cors     = require('cors');
+const path     = require('path');
 const routes   = require('./routes');
 
 const app = express();
@@ -9,7 +11,9 @@ mongoose.connect('mongodb+srv://cone:linux>@cluster0-vg9po.mongodb.net/aircnc?re
     useUnifiedTopology: true
 })
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3333);
