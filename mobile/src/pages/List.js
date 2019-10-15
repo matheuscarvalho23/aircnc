016f344d9ce2e0fcdca384ab422fafc9787d2ebf
Matheus <matheus.carvalho@m2n.com.br>
2019-10-15 18:08:45 -0300

    Login Interface

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import { View, AsyncStorage } from "react-native";
+
+export default function List() {
+
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    })
+  }, [])
+
+  return <View/>
+}
